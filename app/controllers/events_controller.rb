4234b36be3cf86728cfad53fbcd6e7419162d53f
Welison Lucas Almeida Regis <welison.almeida.923@gmail.com>
2018-07-01 19:29:10 -0300

    Possibilidade de adicionar fotos a partir de 'links' da internet incluido ao projeto.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :description, :body, :local, :day, :organization)
+      params.require(:event).permit(:title, :description, :body, :local, :day, :organization, :image_url)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_01_183404) do
+ActiveRecord::Schema.define(version: 2018_07_01_221312) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_07_01_183404) do
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["slug"], name: "index_events_on_slug", unique: true
   end
 
