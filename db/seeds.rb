25b9b3807713661ef1bb270150a49b560ff53e60
Welison Lucas Almeida Regis <welison.almeida.923@gmail.com>
2018-07-02 17:40:10 -0300

    Arquivo de 'seeds' adicionado com exemplos de post e um login para moderador

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,73 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Superuser.create!(email: "superuser1@superuser.com", password: "user123", password_confirmation: "user123")
+
+Event.create!(title: "Carnaval de bloco",
+							description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in ultrices felis. Integer est turpis, aliquet sit amet elit ut, consequat tempus ipsum. Praesent dictum, risus in mollis laoreet, erat velit pellentesque est, suscipit pellentesque lorem neque eu dui.",
+							image_url: "http://regnumchristi.com.br/wp-content/uploads/2018/02/mask-1155808_1280.jpg",
+							body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in ultrices felis. Integer est turpis, aliquet sit amet elit ut, consequat tempus ipsum. Praesent dictum, risus in mollis laoreet, erat velit pellentesque est, suscipit pellentesque lorem neque eu dui. Sed rutrum blandit viverra. Sed mattis ligula quis mi finibus, nec hendrerit neque scelerisque. Phasellus in suscipit tellus. Vivamus nec lacus egestas, dapibus arcu a, tempus erat. Donec sit amet felis elit. Vivamus quis tincidunt enim, nec pharetra felis. Praesent lacus turpis, ornare vitae fringilla non, faucibus in ex. Praesent ultrices, ante quis ultrices gravida, felis nunc vestibulum risus, non rhoncus lorem velit vitae nibh. Cras risus est, rhoncus ut vestibulum nec, laoreet non tellus.
+
+Praesent semper luctus metus faucibus maximus. Mauris sed ante tristique, semper tortor sed, posuere quam. Maecenas fermentum enim tellus, sit amet convallis nibh consectetur non. Donec vitae odio non arcu euismod ornare. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse sit amet gravida ante. Nulla sollicitudin sollicitudin sem a fermentum. Integer congue, quam non imperdiet venenatis, eros velit pharetra massa, ut vehicula mauris nisi quis augue. Aenean suscipit ac orci ac mattis. Proin tempor aliquet quam, vel pharetra ex fermentum in. Phasellus volutpat velit vel felis gravida, sed dignissim magna finibus. Suspendisse potenti. Nullam orci nulla, consequat ut blandit eu, porta vel nisl. Maecenas eu finibus ex. Aenean in justo leo.",
+							local: "DF, Brasília, Eixo Monumental",
+							day: "15/10/2018 09:00:00",
+							organization: "Party Events")
+
+Event.create!(title: "Encontro gamer",
+							description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus a elit eget scelerisque. Maecenas ut arcu id nunc sodales imperdiet quis et purus. In metus erat, laoreet nec mauris eget, porttitor eleifend quam.",
+							image_url: "https://www.blahcultural.com/wp-content/uploads/2016/02/brasil_game_show_ingressos.jpg",
+							body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus a elit eget scelerisque. Maecenas ut arcu id nunc sodales imperdiet quis et purus. In metus erat, laoreet nec mauris eget, porttitor eleifend quam. Praesent sem quam, laoreet quis ligula sit amet, placerat porta lorem. Maecenas ut ultrices enim. Sed eu leo orci. Etiam sed dignissim turpis, quis mattis ipsum. Nullam suscipit interdum convallis. Nunc blandit ante commodo, facilisis augue egestas, malesuada magna. Aenean in dui tortor.
+
+Mauris sit amet eros non est pulvinar aliquam non quis nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus felis est, rutrum sed arcu at, ultricies congue felis. Vivamus laoreet tincidunt bibendum. Nunc pulvinar ex vitae pharetra rhoncus. Donec malesuada eu sem vestibulum suscipit. Ut tempor sollicitudin venenatis. Vestibulum pellentesque quam quis sem lacinia, at volutpat metus porttitor.",
+							local: "DF, SIA treho 7, Guará, feira dos importados, loja 08",
+							day: "2018-02-10 09:00:00",
+							organization: "Gamer Events")
+
+Event.create!(title: "Conversação - Inglês",
+							description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a finibus ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec ipsum enim, vestibulum eu sodales eget, hendrerit nec dui.",
+							image_url: "http://www.universum-ks.org/blog/wp-content/uploads/2017/05/English-1-800x434.jpg",
+							body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a finibus ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec ipsum enim, vestibulum eu sodales eget, hendrerit nec dui. Praesent elementum, arcu non rhoncus molestie, turpis lorem blandit sem, pulvinar facilisis lorem nisl sed nulla. Sed id porta purus. Pellentesque id dolor rhoncus, imperdiet urna vel, aliquet ex. Vestibulum nec gravida mauris. Etiam eget hendrerit nisl. Ut commodo aliquam lorem et porta.
+
+Vestibulum ornare at leo et viverra. Nullam sollicitudin eu dui et tristique. Nam velit nibh, interdum quis hendrerit nec, sollicitudin aliquam risus. Etiam euismod, ante a porta malesuada, diam velit vulputate ante, dapibus fermentum augue nisl at mi. Mauris feugiat dui ac ante suscipit facilisis. Aliquam ultricies tempor tortor, at sagittis neque cursus id. Aenean non magna quis velit mattis vehicula. Cras nec leo id dolor rutrum semper. In malesuada tincidunt scelerisque. Aenean nunc tellus, dignissim id tempor eu, sagittis nec lectus. Pellentesque pulvinar placerat consequat. Phasellus sollicitudin ultricies erat nec sagittis.",
+							local: "UnB Gama, auditório",
+							day: "2018-01-10 10:30:00",
+							organization: "English School")
+
+Event.create!(title: "Dança ao ar livre",
+							description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu lacus eget ex accumsan vehicula id molestie nibh. Duis sed euismod tellus. Vivamus nec quam justo. Mauris mattis mi ac venenatis pretium. Suspendisse ultrices accumsan varius.",
+							image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScF1JhcVwe0YBGWZ4Z14I1tqQiRNdgR0ZRGSfqJHajSZl-ZfxKtw",
+							body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu lacus eget ex accumsan vehicula id molestie nibh. Duis sed euismod tellus. Vivamus nec quam justo. Mauris mattis mi ac venenatis pretium. Suspendisse ultrices accumsan varius. Phasellus elementum eget leo at gravida. In imperdiet sapien eget justo porta, in dictum augue laoreet. In vulputate, nisl at feugiat luctus, velit velit molestie ligula, at commodo diam erat ac tellus.
+
+Donec pellentesque eleifend ante, non congue sapien posuere et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse feugiat mi ligula. Phasellus malesuada tellus vel ipsum viverra ornare. Praesent egestas elit et arcu ullamcorper aliquam. Ut placerat justo varius neque scelerisque, non feugiat felis elementum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam sed elementum risus. Phasellus diam arcu, vestibulum suscipit arcu quis, commodo pharetra nunc. Ut commodo ex non lacus volutpat, eu tempor nibh commodo. Nunc dignissim suscipit elit eget rhoncus. Cras in egestas diam, ut auctor magna. Etiam libero lorem, malesuada vitae suscipit eget, dapibus maximus augue. Quisque finibus varius massa, sit amet facilisis magna aliquet feugiat.",
+							local: "Asa sul, parque da cidade",
+							day: "2018-10-20 11:00:00",
+							organization: "Dance club")
+
+Event.create!(title: "Quarto encontro nerd",
+							description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet tellus nisl. Donec eu ligula sagittis, congue quam ac, ultricies felis. Nullam blandit turpis mi, a accumsan erat dictum eget. Sed porttitor, sapien at venenatis ornare, nisl quam ultrices dolor, euismod efficitur dolor libero lobortis est.",
+							image_url: "https://www.rd.com/wp-content/uploads/2018/02/math-problem-760x506.jpg",
+							body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet tellus nisl. Donec eu ligula sagittis, congue quam ac, ultricies felis. Nullam blandit turpis mi, a accumsan erat dictum eget. Sed porttitor, sapien at venenatis ornare, nisl quam ultrices dolor, euismod efficitur dolor libero lobortis est. Praesent sodales velit sed turpis auctor, nec bibendum nisl laoreet. Mauris sit amet volutpat purus, eu varius leo. Proin in nibh pulvinar, consequat nibh quis, eleifend enim. Pellentesque lacinia molestie felis eget egestas. Aliquam lacinia tellus vitae mi sollicitudin rutrum. Aenean mollis auctor lorem, non congue quam blandit eget. Duis posuere lacus sit amet eros cursus laoreet. Curabitur non elementum ex, ultrices mattis lacus. Nunc consectetur vehicula massa ut vulputate. Ut aliquam, nisi sit amet pharetra hendrerit, turpis massa rutrum ipsum, in gravida sem lectus in lacus. Nam efficitur tincidunt diam sit amet commodo. Phasellus quis porta felis, sit amet semper sapien.
+
+Nullam suscipit lectus at neque venenatis, sed ornare mauris efficitur. Nam at semper mi. Aliquam pellentesque varius dolor, sed suscipit urna fermentum non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam fringilla tempus tempus. Sed ac ornare ipsum, et congue velit. Praesent mollis sem sed purus posuere, in eleifend sapien sodales. Aliquam erat volutpat. Phasellus imperdiet felis et lectus dignissim, id dictum odio bibendum. Praesent felis sem, placerat at maximus ut, fringilla sed massa. Suspendisse quis blandit neque. Morbi id dolor ante. Phasellus viverra non risus eu bibendum.",
+							local: "UnB, Instituto de Matemática",
+							day: "2018-10-19 11:00:00",
+							organization: "NerdLife")
+
+Event.create!(title: "Competição de Counter-Strike",
+							description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a nisi porta, luctus erat vitae, maximus neque. In sagittis auctor elit. Nullam eleifend sed lectus id mattis. In eleifend luctus quam at pellentesque. Quisque vitae sapien est. Nunc tristique egestas porta. Donec ac nunc nec enim feugiat mollis. Suspendisse vel hendrerit orci.",
+							image_url: "https://www.gramno.com/image/cache/catalog/Products/counter_strike_globa_OzboC-460x215.jpg",
+							body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a nisi porta, luctus erat vitae, maximus neque. In sagittis auctor elit. Nullam eleifend sed lectus id mattis. In eleifend luctus quam at pellentesque. Quisque vitae sapien est. Nunc tristique egestas porta. Donec ac nunc nec enim feugiat mollis. Suspendisse vel hendrerit orci.
+
+Duis pellentesque felis eget ultrices faucibus. Integer ornare eleifend enim, ut lacinia purus elementum ac. Cras laoreet, quam at varius porttitor, risus magna mattis augue, quis iaculis nunc metus sit amet felis. Sed malesuada feugiat augue. Nullam et eleifend ex. Etiam volutpat ex aliquam porta vulputate. Quisque quis bibendum risus. Nullam pretium sodales pellentesque. Nullam elementum, nibh nec scelerisque tincidunt, metus est facilisis justo, nec fermentum tellus erat in est. Maecenas ut leo in ligula egestas varius. Curabitur at ex quam. Nulla facilisi. Curabitur cursus arcu sit amet molestie dapibus.
+
+Donec ullamcorper dolor eget iaculis porta. Nunc vitae ante vulputate, sagittis risus a, finibus tellus. Quisque viverra congue quam, tincidunt blandit nisl sagittis nec. Etiam feugiat lectus nec posuere pharetra. Nam rhoncus egestas enim, eget faucibus purus convallis eu. Curabitur sit amet ex a nisl feugiat aliquam. Sed eget massa mi. Curabitur et tristique ligula. Praesent in leo interdum, blandit massa at, facilisis nulla. Vestibulum nulla lorem, euismod eget tincidunt a, commodo vitae erat.",
+							local: "DF, Gama, Lan house Arena - Q. 51, casa 19, st. leste",
+							day: "2018-09-25 11:20:00",
+							organization: "NerdLife")
+
+Event.create!(title: "Décimo encontro de jogadores de 'Pokemon Go'",
+							description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean suscipit sed massa eget aliquet. Etiam erat tellus, rutrum sit amet tincidunt vel, tincidunt non turpis. Aenean pretium maximus tincidunt. Vestibulum tincidunt, metus in facilisis finibus, felis ex ultrices orci, et pulvinar nunc tellus sit amet erat. Integer malesuada sodales consectetur. Pellentesque laoreet pharetra hendrerit. Proin ac ultrices augue. Aliquam erat volutpat.",
+							image_url: "https://www.bleedingcool.com/wp-content/uploads/2017/02/pokemongo-go-169.jpg",
+							body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean suscipit sed massa eget aliquet. Etiam erat tellus, rutrum sit amet tincidunt vel, tincidunt non turpis. Aenean pretium maximus tincidunt. Vestibulum tincidunt, metus in facilisis finibus, felis ex ultrices orci, et pulvinar nunc tellus sit amet erat. Integer malesuada sodales consectetur. Pellentesque laoreet pharetra hendrerit. Proin ac ultrices augue. Aliquam erat volutpat. Cras at sodales massa. Praesent felis tellus, suscipit eu orci vitae, efficitur hendrerit ligula. Etiam ut nulla lacinia, facilisis sem aliquam, egestas enim. Praesent nec turpis quis massa ultrices vehicula eget eu ante. Maecenas consectetur scelerisque lobortis. Praesent porttitor bibendum mi, id pellentesque nisi malesuada et. Vestibulum ac nunc sit amet diam efficitur dapibus. Donec rhoncus eget tellus et imperdiet.
+
+Praesent nec pulvinar libero. Nullam ligula est, posuere venenatis nulla a, posuere pharetra massa. Etiam non dui quis magna accumsan interdum vitae quis erat. Sed in urna eu dui molestie iaculis. Nulla vitae vehicula nulla, in placerat orci. In sem mauris, mollis a nisi vitae, facilisis sagittis risus. Cras velit odio, eleifend a accumsan id, mattis ac neque. In pellentesque dignissim ex quis feugiat. Duis auctor nisi turpis, sed molestie nisi fringilla at. Vestibulum tristique turpis in lectus consectetur cursus. Aenean rhoncus dignissim diam, id efficitur purus. Etiam et tristique leo. Vestibulum a feugiat velit. Phasellus eu semper velit, sit amet hendrerit dolor.",
+							local: "Asa sul, parque da cidade",
+							day: "2018-08-20 23:30:00",
+							organization: "NerdLife")
