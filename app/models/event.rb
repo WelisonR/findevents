dcb81f7a20c9e341803db53b65639aa85746185f
Welison Lucas Almeida Regis <welison.almeida.923@gmail.com>
2018-07-02 21:26:03 -0300

    Passa a ser obrigatório digitar as informações do formulário para criar um evento.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,21 @@
 class Event < ApplicationRecord
 
+    # Generate special links to events
     extend FriendlyId
     friendly_id :title, use: :slugged
 
+    # Sort the 'events'
     scope :most_recent, -> {order(id: :desc)}
 
+    # User inputs validation
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :body, presence: true
+    validates :local, presence: true
+    validates :day, presence: true
+    validates :organization, presence: true
+
+    # Update the link if it is necessary
     def should_generate_new_friendly_id?
         title_changed?
     end
